f5d15cd64f1bff13829c70e5d893cb9b0f1e8e29
JustBarnt <justbarnt@icloud.com>
2022-08-28 22:49:25 -0400

    File creation

---
+++ src/components/CreateAccount.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import paseto from "paseto";
+
+	const { decode } = paseto;
+	const { V3 } = paseto;
+	const { errors } = paseto;
+</script>
---
+++ src/components/Login.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	let username: string;
+	let password: string;
+	let attempts:number = 3;
+</script>
