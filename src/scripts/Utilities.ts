20066244565e5e891ad2f768b60138b41162d480
JustBarnt <justbarnt@icloud.com>
2022-09-03 22:17:27 -0400

    Created a sanitization function

---
+++ src/scripts/Utilities.ts
@@ -0,0 +1,27 @@
+enum EntityMap 
+{
+	"&" = "&amp;",
+	"<" = "&lt;",
+	">" = "&gt;",
+	"\"" = "&quot;",
+	"'" = "&#39;",
+	"/" = "&#x2F;",
+	"`" = "&#x60;",
+	"=" = "&#x3D;"
+}
+
+class Utils
+{
+	/**
+	* Replaces a potentially hazardous string with a normal string with no HTML.
+	* @param {String} input - User input.
+	* @return {String} A sanitized string.
+	*/
+	static SanitizeInput(input: string)
+	{
+		return String(string).replace(/[&<>"'`=\/]/g, (s) => 
+		{
+			return EntityMap[s];
+		});
+	}
+}
